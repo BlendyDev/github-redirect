157a395e8977486f54089a3044998dd76a1ff3bc
BlendyDev <github@blendy.dev>
2024-11-09 17:21:06 +0100

    feat: opengraph meta generator GH actions

---
+++ create-html.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+URL="https://github.com/BlendyDev"
+data=$(curl -s "$URL")
+tags=$(echo "$data" | grep "property=\"og:")
+title=$(echo "$data" | grep "<title>")
+cat << EOF > dist/index.html
+<!DOCTYPE html>
+<html>
+  <head>
+    $title
+    <meta charset="UTF-8">
+    $tags
+    <meta http-equiv="refresh" content="0;url=https://github.com/blendydev" />
+    <link rel="canonical" href="$URL" />
+  </head>
+  <body>
+    <p>redirecting...</p>
+  </body>
+</html>
+EOF
+echo "HTML generated"
